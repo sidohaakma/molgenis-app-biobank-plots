2eb86cc1ffa629670ce80b0ae7bd9ce487c1adc0
Mark-de-Haan <markdehaan90@gmail.com>
2018-03-27 13:52:36 +0200

    Refactor main.js imports

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import store from './store'
-import router from './router'
 import Vue from 'vue'
 import App from './App'
+
+import store from './store'
+import router from './router'
 
 Vue.config.productionTip = false
 
@@ -12,6 +11,6 @@ new Vue({
   el: '#app',
   store,
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
 })
