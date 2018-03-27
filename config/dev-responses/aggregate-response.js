708e18ac80605091cca89960f609e7a0168a99be
Mark-de-Haan <markdehaan90@gmail.com>
2018-03-27 17:25:11 +0200

    Add aggregate response, remove getters

---
+++ config/dev-responses/aggregate-response.js
@@ -0,0 +1,209 @@
+module.exports = {
+  'aggs': {
+    'matrix': [
+      [
+        877
+      ],
+      [
+        649
+      ],
+      [
+        128
+      ],
+      [
+        979
+      ],
+      [
+        337
+      ],
+      [
+        259
+      ],
+      [
+        855
+      ],
+      [
+        995
+      ],
+      [
+        500
+      ],
+      [
+        435
+      ],
+      [
+        500
+      ],
+      [
+        1815
+      ],
+      [
+        3393
+      ],
+      [
+        90
+      ],
+      [
+        1600
+      ],
+      [
+        4366
+      ],
+      [
+        193
+      ],
+      [
+        983
+      ],
+      [
+        109
+      ],
+      [
+        5303
+      ],
+      [
+        331
+      ],
+      [
+        98
+      ],
+      [
+        234
+      ],
+      [
+        1198
+      ]
+    ],
+    'xLabels': [
+      {
+        'id': 'ALPHAOMEGA',
+        'abbr': 'ALPHAOMEGA'
+      },
+      {
+        'id': 'BIOMARCS',
+        'abbr': 'BIOMARCS'
+      },
+      {
+        'id': 'CHARM',
+        'abbr': 'CHARM'
+      },
+      {
+        'id': 'CHECK',
+        'abbr': 'CHECK'
+      },
+      {
+        'id': 'CODAM',
+        'abbr': 'CODAM'
+      },
+      {
+        'id': 'CSF',
+        'abbr': 'CSF'
+      },
+      {
+        'id': 'DMS',
+        'abbr': 'DMS'
+      },
+      {
+        'id': 'DZS_WF',
+        'abbr': 'DZS_WF'
+      },
+      {
+        'id': 'FUNCTGENOMICS',
+        'abbr': 'FUNCTGENOMICS'
+      },
+      {
+        'id': 'GARP',
+        'abbr': 'GARP'
+      },
+      {
+        'id': 'HELIUS',
+        'abbr': 'HELIUS'
+      },
+      {
+        'id': 'LL',
+        'abbr': 'LL'
+      },
+      {
+        'id': 'LLS',
+        'abbr': 'LLS'
+      },
+      {
+        'id': 'MRS',
+        'abbr': 'MRS'
+      },
+      {
+        'id': 'NESDA',
+        'abbr': 'NESDA'
+      },
+      {
+        'id': 'NTR',
+        'abbr': 'NTR'
+      },
+      {
+        'id': 'PAN',
+        'abbr': 'PAN'
+      },
+      {
+        'id': 'PROSPER',
+        'abbr': 'PROSPER'
+      },
+      {
+        'id': 'RAAK',
+        'abbr': 'RAAK'
+      },
+      {
+        'id': 'RS',
+        'abbr': 'RS'
+      },
+      {
+        'id': 'STEMI_GIPS-III',
+        'abbr': 'STEMI_GIPS-III'
+      },
+      {
+        'id': 'TACTICS',
+        'abbr': 'TACTICS'
+      },
+      {
+        'id': 'TOMAAT',
+        'abbr': 'TOMAAT'
+      },
+      {
+        'id': 'UCORBIO',
+        'abbr': 'UCORBIO'
+      }
+    ],
+    'yLabels': []
+  },
+  'xAttr': {
+    'href': '/api/v2/leiden_RP/meta/biobank',
+    'fieldType': 'CATEGORICAL',
+    'name': 'biobank',
+    'label': 'Biobank',
+    'attributes': [],
+    'refEntity': {
+      'href': '/api/v2/leiden_biobanks',
+      'hrefCollection': '/api/v2/leiden_biobanks',
+      'name': 'leiden_biobanks',
+      'label': 'biobanks',
+      'attributes': [],
+      'labelAttribute': 'abbr',
+      'idAttribute': 'id',
+      'lookupAttributes': [
+        'id',
+        'abbr'
+      ],
+      'isAbstract': false,
+      'writable': true,
+      'languageCode': 'en'
+    },
+    'auto': false,
+    'nillable': false,
+    'readOnly': false,
+    'labelAttribute': false,
+    'unique': false,
+    'visible': true,
+    'lookupAttribute': false,
+    'isAggregatable': true
+  },
+  'href': '/api/v2/leiden_RP'
+}
