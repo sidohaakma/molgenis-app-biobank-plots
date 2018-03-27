fef64f52178d7423432af8dbfc9a09a01ad95a3f
Mark-de-Haan <markdehaan90@gmail.com>
2018-03-27 15:05:17 +0200

    Add RangeFilterComponent

--- src/components/FilterComponent.vue
+++ src/components/FilterComponent.vue
@@ -1,21 +1,31 @@
 <template>
   <div class="filter-item">
     <h5>{{filter.label}}</h5>
-    <dropdown-filter-component
-      v-if="filter.filterType === 'dropdown'"
-      :options="filter.options">
-    </dropdown-filter-component>
+
+    <template v-if="filter.filterType === 'dropdown'">
+      <dropdown-filter-component
+        :onOptionSelect="onOptionSelect"
+        :initialSelectedOption="selectedBiobank"
+        :options="filter.options">
+      </dropdown-filter-component>
+    </template>
+
+    <template v-if="filter.filterType === 'range'">
+      <range-filter-component></range-filter-component>
+    </template>
   </div>
 </template>
 
 <script>
   import DropdownFilterComponent from './DropdownFilterComponent'
+  import RangeFilterComponent from './RangeFilterComponent'
 
   export default {
     name: 'FilterComponent',
     props: ['filter'],
     components: {
-      DropdownFilterComponent
+      DropdownFilterComponent,
+      RangeFilterComponent
     }
   }
 </script>
