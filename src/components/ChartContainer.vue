2c1781e0dbe351b81c3ce32cd3cf5e963a4912a0
Mark-de-Haan <markdehaan90@gmail.com>
2018-03-29 16:54:19 +0200

    Replace vue-charts with vue-chartjs

--- src/components/ChartContainer.vue
+++ src/components/ChartContainer.vue
@@ -1,46 +1,28 @@
 <template>
   <div class="chart-container">
-    <template v-for="attribute in attributeChartData">
+    <div v-for="attribute in attributeChartData" class="mb-2 chart-component py-3">
       <chart-component
         :data="attribute.data"
         :chartType="attribute.chartType"
         :labels="attribute.labels"
         :options="attribute.options">
       </chart-component>
-    </template>
+    </div>
   </div>
 </template>
 
 <style scoped>
-  .chart-container {
-    box-shadow: 5px 5px 7px 5px #d7dae0;
+  .chart-component {
+    box-shadow: -1px 6px 7px #d7dae0;
     border: none;
   }
 </style>
 
 <script>
-  import Vue from 'vue'
-  import VueCharts from 'vue-charts'
-
   import ChartComponent from './chart-components/ChartComponent'
 
-  Vue.use(VueCharts)
   export default {
     name: 'ChartContainer',
-    data () {
-      return {
-        data: [
-          ['2004', 1],
-          ['2005', 2],
-          ['2006', 3],
-          ['2007', 4]
-        ],
-        labels: [
-          {'type': 'string', 'label': 'Year'},
-          {'type': 'number', 'label': 'Costs'}
-        ]
-      }
-    },
     computed: {
       attributeChartData () {
         return this.$store.state.attributeChartData
