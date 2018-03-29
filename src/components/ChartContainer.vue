4fd205d21404786eb38a6ae4febd8d7b69d46cc9
Mark-de-Haan <markdehaan90@gmail.com>
2018-03-29 14:32:06 +0200

    Add ChartContainer and ChartComponent

---
+++ src/components/ChartContainer.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="chart-container">
+    <template v-for="attribute in attributeChartData">
+      <chart-component
+        :data="attribute.data"
+        :chartType="attribute.chartType"
+        :labels="attribute.labels"
+        :options="attribute.options">
+      </chart-component>
+    </template>
+  </div>
+</template>
+
+<style scoped>
+  .chart-container {
+    box-shadow: 5px 5px 7px 5px #d7dae0;
+    border: none;
+  }
+</style>
+
+<script>
+  import Vue from 'vue'
+  import VueCharts from 'vue-charts'
+
+  import ChartComponent from './chart-components/ChartComponent'
+
+  Vue.use(VueCharts)
+  export default {
+    name: 'ChartContainer',
+    data () {
+      return {
+        data: [
+          ['2004', 1],
+          ['2005', 2],
+          ['2006', 3],
+          ['2007', 4]
+        ],
+        labels: [
+          {'type': 'string', 'label': 'Year'},
+          {'type': 'number', 'label': 'Costs'}
+        ]
+      }
+    },
+    computed: {
+      attributeChartData () {
+        return this.$store.state.attributeChartData
+      }
+    },
+    components: {
+      ChartComponent
+    }
+  }
+</script>
---
+++ src/components/chart-components/ChartComponent.vue
@@ -0,0 +1,43 @@
+<template>
+  <vue-chart
+    :chartType="chartType"
+    :columns="columns"
+    :rows="rows"
+    :options="options">
+  </vue-chart>
+</template>
+
+<script>
+  export default {
+    name: 'ChartComponent',
+    props: {
+      chartType: {
+        type: String,
+        required: false,
+        default: 'LineChart'
+      },
+      data: {
+        type: Array,
+        required: false,
+        default: () => []
+      },
+      labels: {
+        type: Array,
+        required: true
+      },
+      options: {
+        type: Object,
+        required: false,
+        default: () => ({})
+      }
+    },
+    computed: {
+      columns () {
+        return this.labels
+      },
+      rows () {
+        return this.data
+      }
+    }
+  }
+</script>
