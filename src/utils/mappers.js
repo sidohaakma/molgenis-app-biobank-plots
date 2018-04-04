eadc0c74bc88008ab3b41c0963abe93036309f3d
Mark-de-Haan <markdehaan90@gmail.com>
2018-04-04 11:17:13 +0200

    Add activeFilterToRSQL mapper + unit test

--- src/utils/mappers.js
+++ src/utils/mappers.js
@@ -151,6 +151,24 @@ export const aggregateDataToChartData = (attribute, aggregates) => {
   }
 }
 
+/**
+ * Map activeFilters object to an RSQL string
+ * Very specific for the data_types filter because every filter is actually an attribute
+ * Very specific for the age_years filter because there is a min and a max value
+ */
 export const mapActiveFiltersToRSQL = (activeFilters) => {
-  return 'sex==male'
+  return Object.keys(activeFilters).reduce((accumulator, filter) => {
+    const values = activeFilters[filter]
+    if (filter === 'age_years') {
+      accumulator.push(filter + '=ge=' + values.min)
+      accumulator.push(filter + '=le=' + values.max)
+    } else if (filter === 'data_type') {
+      values.forEach(value => accumulator.push(value + '==true'))
+    } else if (Array.isArray(values)) {
+      values.forEach(value => accumulator.push(filter + '==' + value))
+    } else {
+      accumulator.push(filter + '==' + values)
+    }
+    return accumulator
+  }, []).join(';')
 }
--- test/unit/specs/util/mappers.spec.js
+++ test/unit/specs/util/mappers.spec.js
@@ -1,9 +1,29 @@
-// import * as mappers from 'src/utils/mappers'
+import * as mappers from 'src/utils/mappers'
 
 describe('mappers', () => {
   describe('sampleToCategoriesMapper', () => {
     it('should map metadata to a category filter map', () => {
       // test
+    })
+  })
+
+  describe('mapActiveFiltersToRSQL', () => {
+    it('should map an object of filters to an RSQL string', () => {
+      const activeFilters = {
+        biobank: 'CHECK',
+        sex: ['male'],
+        smoking: [true],
+        age_years: {
+          min: 25,
+          max: 50
+        },
+        data_type: ['genotypes', 'metabolome', 'methylome']
+      }
+
+      const actual = mappers.mapActiveFiltersToRSQL(activeFilters)
+      const expected = 'biobank==CHECK;sex==male;smoking==true;age_years=ge=25;age_years=le=50;genotypes==true;metabolome==true;methylome==true'
+
+      expect(actual).to.equal(expected)
     })
   })
 })
