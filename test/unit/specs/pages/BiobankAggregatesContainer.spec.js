84b87addfa74ca6d373760b42718759fc1a9c1c6
Mark-de-Haan <markdehaan90@gmail.com>
2018-04-05 10:57:13 +0200

    Add pages component tests

---
+++ test/unit/specs/pages/BiobankAggregatesContainer.spec.js
@@ -0,0 +1,47 @@
+import BiobankAggregatesContainer from 'src/pages/BiobankAggregatesContainer'
+import { shallow, createLocalVue } from '@vue/test-utils'
+import td from 'testdouble'
+import Vuex from 'vuex'
+
+describe('pages', () => {
+  describe('BiobankAggregatesContainer', () => {
+    let actions
+    let localVue
+    let state
+    let store
+
+    beforeEach(() => {
+      localVue = createLocalVue()
+      localVue.use(Vuex)
+
+      actions = {
+        GET_SUBJECT_METADATA: td.function(),
+        GET_SUBJECT_AGGREGATION: td.function(),
+        UPDATE_SUBJECT_AGGREGATION: td.function()
+      }
+
+      state = {
+        activeFilters: {}
+      }
+
+      store = new Vuex.Store({actions, state})
+    })
+
+    it('should dispatch GET_SUBJECT_METADATA and GET_SUBJECT_AGGREGATION when created', () => {
+      shallow(BiobankAggregatesContainer, {store, localVue})
+      td.verify(actions.GET_SUBJECT_METADATA(td.matchers.anything(), undefined, undefined))
+      td.verify(actions.GET_SUBJECT_AGGREGATION(td.matchers.anything(), undefined, undefined))
+    })
+
+    it('should compute activeFilters based on the store', () => {
+      const wrapper = shallow(BiobankAggregatesContainer, {store, localVue})
+      expect(wrapper.vm.activeFilters).to.deep.equal(state.activeFilters)
+    })
+
+    it('should watch the activeFilters and dispatch UPDATE_SUBJECT_AGGREGATION on change', () => {
+      shallow(BiobankAggregatesContainer, {store, localVue})
+      store.state.activeFilters = {filter: 'value'}
+      td.verify(actions.UPDATE_SUBJECT_AGGREGATION(td.matchers.anything(), undefined, undefined))
+    })
+  })
+})
