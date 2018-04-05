3485810c05fd11b0640bd50db939346c3e85221a
Mark-de-Haan <markdehaan90@gmail.com>
2018-04-05 10:57:37 +0200

    Group mapper tests under a single describe

--- test/unit/specs/mappers/active-filters-to-rsql-mapper.spec.js
+++ test/unit/specs/mappers/active-filters-to-rsql-mapper.spec.js
@@ -1,18 +1,20 @@
 import activeFilterToRsqlMapper from 'src/mappers/active-filters-to-rsql-mapper'
 
-describe('mapActiveFiltersToRSQL', () => {
-  it('should map an object of filters to an RSQL string', () => {
-    const activeFilters = {
-      biobank: 'CHECK',
-      sex: ['male'],
-      smoking: [true],
-      age_years: [25, 50],
-      data_type: ['genotypes', 'metabolome', 'methylome']
-    }
+describe('mappers', () => {
+  describe('mapActiveFiltersToRSQL', () => {
+    it('should map an object of filters to an RSQL string', () => {
+      const activeFilters = {
+        biobank: 'CHECK',
+        sex: ['male'],
+        smoking: [true],
+        age_years: [25, 50],
+        data_type: ['genotypes', 'metabolome', 'methylome']
+      }
 
-    const actual = activeFilterToRsqlMapper(activeFilters)
-    const expected = 'biobank==CHECK;sex==male;smoking==true;age_years=ge=25;age_years=le=50;genotypes==true;metabolome==true;methylome==true'
+      const actual = activeFilterToRsqlMapper(activeFilters)
+      const expected = 'biobank==CHECK;sex==male;smoking==true;age_years=ge=25;age_years=le=50;genotypes==true;metabolome==true;methylome==true'
 
-    expect(actual).to.equal(expected)
+      expect(actual).to.equal(expected)
+    })
   })
 })
--- test/unit/specs/mappers/aggregate-data-to-chart-data-mapper.spec.js
+++ test/unit/specs/mappers/aggregate-data-to-chart-data-mapper.spec.js
@@ -2,180 +2,182 @@ import aggregateDataToChartDataMapper from 'src/mappers/aggregate-data-to-chart-
 
 const initialState = window.__INITIAL_STATE__ || {}
 
-describe('aggregateDataToChartDataMapper', () => {
-  it('should map an aggregate result to a chart data object for a horizontal bar chart', () => {
-    const attribute = {
-      name: 'biobank',
-      title: 'Number of samples per biobank',
-      type: 'HorizontalBarChart',
-      inline: false,
-      sorted: false
-    }
+describe('mappers', () => {
+  describe('aggregateDataToChartDataMapper', () => {
+    it('should map an aggregate result to a chart data object for a horizontal bar chart', () => {
+      const attribute = {
+        name: 'biobank',
+        title: 'Number of samples per biobank',
+        type: 'HorizontalBarChart',
+        inline: false,
+        sorted: false
+      }
 
-    const aggregates = {
-      matrix: [
-        [877],
-        [649]
-      ],
-      xLabels: [
-        {
-          'id': 'ALPHAOMEGA',
-          'abbr': 'ALPHAOMEGA'
-        },
-        {
-          'id': 'BIOMARCS',
-          'abbr': 'BIOMARCS'
-        }
-      ]
-    }
-
-    const actual = aggregateDataToChartDataMapper(attribute, aggregates)
-    const expected = {
-      'dataCollection': {
-        'datasets': [
-          {
-            'backgroundColor': initialState.primaryColor,
-            'data': [
-              877,
-              649
-            ]
-          }
+      const aggregates = {
+        matrix: [
+          [877],
+          [649]
         ],
-        'labels': [
-          'ALPHAOMEGA',
-          'BIOMARCS'
+        xLabels: [
+          {
+            'id': 'ALPHAOMEGA',
+            'abbr': 'ALPHAOMEGA'
+          },
+          {
+            'id': 'BIOMARCS',
+            'abbr': 'BIOMARCS'
+          }
         ]
-      },
-      'id': 'biobank',
-      'inline': false,
-      'title': 'Number of samples per biobank',
-      'type': 'HorizontalBarChart'
-    }
+      }
 
-    expect(actual).to.deep.equal(expected)
-  })
+      const actual = aggregateDataToChartDataMapper(attribute, aggregates)
+      const expected = {
+        'dataCollection': {
+          'datasets': [
+            {
+              'backgroundColor': initialState.primaryColor,
+              'data': [
+                877,
+                649
+              ]
+            }
+          ],
+          'labels': [
+            'ALPHAOMEGA',
+            'BIOMARCS'
+          ]
+        },
+        'id': 'biobank',
+        'inline': false,
+        'title': 'Number of samples per biobank',
+        'type': 'HorizontalBarChart'
+      }
 
-  it('should map an aggregate result to a chart data object for a vertical bar chart', () => {
-    const attribute = {
-      name: 'age_years',
-      title: 'Age',
-      type: 'VerticalBarChart',
-      inline: false,
-      sorted: true
-    }
+      expect(actual).to.deep.equal(expected)
+    })
 
-    const aggregates = {
-      matrix: [
-        [877],
-        [649],
-        [231]
-      ],
-      xLabels: [
-        25,
-        50,
-        null
-      ]
-    }
+    it('should map an aggregate result to a chart data object for a vertical bar chart', () => {
+      const attribute = {
+        name: 'age_years',
+        title: 'Age',
+        type: 'VerticalBarChart',
+        inline: false,
+        sorted: true
+      }
 
-    const actual = aggregateDataToChartDataMapper(attribute, aggregates)
-    const expected = {
-      'dataCollection': {
-        'datasets': [
-          {
-            'backgroundColor': initialState.primaryColor,
-            'data': [
-              877,
-              649,
-              231
-            ]
-          }
+      const aggregates = {
+        matrix: [
+          [877],
+          [649],
+          [231]
         ],
-        'labels': [
+        xLabels: [
           25,
           50,
-          'Unknown'
+          null
         ]
-      },
-      'id': 'age_years',
-      'inline': false,
-      'title': 'Age',
-      'type': 'VerticalBarChart'
-    }
+      }
 
-    expect(actual).to.deep.equal(expected)
-  })
+      const actual = aggregateDataToChartDataMapper(attribute, aggregates)
+      const expected = {
+        'dataCollection': {
+          'datasets': [
+            {
+              'backgroundColor': initialState.primaryColor,
+              'data': [
+                877,
+                649,
+                231
+              ]
+            }
+          ],
+          'labels': [
+            25,
+            50,
+            'Unknown'
+          ]
+        },
+        'id': 'age_years',
+        'inline': false,
+        'title': 'Age',
+        'type': 'VerticalBarChart'
+      }
 
-  it('should map an aggregate result to a chart data object for a column chart', () => {
-    const attribute = {
-      name: 'smoking',
-      inline: true,
-      title: 'Smoking data',
-      type: 'ColumnChart',
-      datasets: [
-        {label: 'Yes', backgroundColor: initialState.primaryColor},
-        {label: 'No', backgroundColor: initialState.secondaryColor},
-        {label: 'Not available', backgroundColor: initialState.tertiaryColor}
-      ]
-    }
+      expect(actual).to.deep.equal(expected)
+    })
 
-    const aggregates = {
-      matrix: [
-        [877],
-        [649],
-        [532]
-      ],
-      xLabels: [
-        true,
-        false,
-        null
-      ]
-    }
+    it('should map an aggregate result to a chart data object for a column chart', () => {
+      const attribute = {
+        name: 'smoking',
+        inline: true,
+        title: 'Smoking data',
+        type: 'ColumnChart',
+        datasets: [
+          {label: 'Yes', backgroundColor: initialState.primaryColor},
+          {label: 'No', backgroundColor: initialState.secondaryColor},
+          {label: 'Not available', backgroundColor: initialState.tertiaryColor}
+        ]
+      }
 
-    const actual = aggregateDataToChartDataMapper(attribute, aggregates)
-    const expected = {
-      'dataCollection': {
-        'datasets': [
-          {
-            'backgroundColor': initialState.primaryColor,
-            'data': [
-              877
-            ],
-            'label': 'Yes'
-          },
-          {
-            'backgroundColor': initialState.secondaryColor,
-            'data': [
-              649
-            ],
-            'label': 'No'
-          },
-          {
-            'backgroundColor': initialState.tertiaryColor,
-            'data': [
-              532
-            ],
-            'label': 'Not available'
-          }
+      const aggregates = {
+        matrix: [
+          [877],
+          [649],
+          [532]
         ],
-        'labels': [
-          'Smoking data'
+        xLabels: [
+          true,
+          false,
+          null
         ]
-      },
-      'id': 'smoking',
-      'inline': true,
-      'title': 'Smoking data',
-      'type': 'ColumnChart'
-    }
+      }
 
-    expect(actual).to.deep.equal(expected)
-  })
+      const actual = aggregateDataToChartDataMapper(attribute, aggregates)
+      const expected = {
+        'dataCollection': {
+          'datasets': [
+            {
+              'backgroundColor': initialState.primaryColor,
+              'data': [
+                877
+              ],
+              'label': 'Yes'
+            },
+            {
+              'backgroundColor': initialState.secondaryColor,
+              'data': [
+                649
+              ],
+              'label': 'No'
+            },
+            {
+              'backgroundColor': initialState.tertiaryColor,
+              'data': [
+                532
+              ],
+              'label': 'Not available'
+            }
+          ],
+          'labels': [
+            'Smoking data'
+          ]
+        },
+        'id': 'smoking',
+        'inline': true,
+        'title': 'Smoking data',
+        'type': 'ColumnChart'
+      }
 
-  it('Should throw an UnsupportedChartTypeException when there is an unknown chart type', () => {
-    const attribute = {
-      type: 'Unknown chart type'
-    }
+      expect(actual).to.deep.equal(expected)
+    })
 
-    const aggregates = {}
-    expect(() => aggregateDataToChartDataMapper(attribute, aggregates)).to.throw('Unsupported chart type: Unknown chart type')
+    it('Should throw an UnsupportedChartTypeException when there is an unknown chart type', () => {
+      const attribute = {
+        type: 'Unknown chart type'
+      }
+
+      const aggregates = {}
+      expect(() => aggregateDataToChartDataMapper(attribute, aggregates)).to.throw('Unsupported chart type: Unknown chart type')
+    })
   })
 })
