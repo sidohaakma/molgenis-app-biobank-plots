f1061b4b13c3659174ee7fc162b6c08c4ab5af45
Mark-de-Haan <markdehaan90@gmail.com>
2018-04-17 11:21:16 +0200

    Fix router, main, and index.html to be store compatible Include app store files

--- config/index.js
+++ config/index.js
@@ -39,6 +39,7 @@ module.exports = {
     // If true, your code will be linted during bundling and
     // linting errors and warnings will be shown in the console.
     useEslint: true,
+
     // If true, eslint errors and warnings will also be shown in the error overlay
     // in the browser.
     showEslintErrorsInOverlay: false,
@@ -54,9 +55,7 @@ module.exports = {
     // set this to false - it *may* help
     // https://vue-loader.vuejs.org/en/options.html#cachebusting
     cacheBusting: true,
-
     cssSourceMap: true,
-
     before (app) {
       app.get('/api/v2/leiden_RP', function (request, response) {
         if (request.query.aggs) {
@@ -85,37 +84,28 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot:
-      path.resolve(__dirname, '../dist'),
-    assetsSubDirectory:
-      'static',
-    assetsPublicPath:
-      '/',
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsSubDirectory: '',
+    assetsPublicPath: '/apps/${app.id}',
 
     /**
      * Source Maps
      */
-
-    productionSourceMap:
-      true,
+    productionSourceMap: true,
     // https://webpack.js.org/configuration/devtool/#production
-    devtool:
-      '#source-map',
+    devtool: '#source-map',
 
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
     // npm install --save-dev compression-webpack-plugin
-    productionGzip:
-      false,
-    productionGzipExtensions:
-      ['js', 'css'],
+    productionGzip: false,
+    productionGzipExtensions: ['js', 'css'],
 
     // Run the build command with an extra argument to
     // View the bundle analyzer report after build finishes:
     // `npm run build --report`
     // Set to `true` or `false` to always turn it on or off
-    bundleAnalyzerReport:
-    process.env.npm_config_report
+    bundleAnalyzerReport: process.env.npm_config_report
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import BiobankAggregatesContainer from 'src/pages/BiobankAggregatesContainer'
 
+const {baseUrl} = window.__INITIAL_STATE__ || {}
+
 Vue.use(Router)
 export default new Router({
   mode: 'history',
+  base: baseUrl,
   routes: [
     {
       path: '/',
